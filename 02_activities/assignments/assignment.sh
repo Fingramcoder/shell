468a924a17887f4ed7fffbae66125684b57dec74
Nitin Puri <nitinpuri.iift@gmail.com>
2025-11-16 00:16:56 -0500

    third attempt

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,8 +52,8 @@ find ./data/raw -type f -name "*event*.log" -exec cp {} ./data/processed/event_l
 #cp ./data/raw/*user*.log ./data/processed/user_logs/
 #cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-find ./data/raw -type f -name "*ipaddr*" -exec git rm -f {} +
-find ./data/processed/user_logs -type f -name "*ipaddr*" -exec git rm -f {} +
+find ./data/raw -type f -name "*ipaddr*" -exec git rm -f --ignore-unmatch {} +
+find ./data/processed/user_logs -type f -name "*ipaddr*" -exec git rm -f --ignore-unmatch {} +
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
