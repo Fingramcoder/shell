2a913875847e83274a3b8bdf2ef0863d86716f9a
Nitin Puri <nitinpuri.iift@gmail.com>
2025-11-16 00:27:15 -0500

    5 attmpt

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,7 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir -p ./data
+#mkdir -p ./data
 
 git add -f ./rawdata
 git mv ./rawdata ./data/raw
