2d40978643973ccc3231c17a2ab96045569a7669
Nitin Puri <nitinpuri.iift@gmail.com>
2025-11-16 00:49:00 -0500

    8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,9 +31,9 @@ unzip -q rawdata.zip
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 #mkdir -p ./data
-rm -rf ./data/raw
-git add -f ./
-git mv ./rawdata ./data/raw
+#rm -rf ./data/raw
+#git add -f ./
+mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
